6b36ebfef0e9181582be68a1f453a924015f93f6
Aniket Pratap Singh <103597886+createaniket@users.noreply.github.com>
2023-02-10 01:02:22 +0530

    some style added

--- src/Component/Productdetail.jsx
+++ src/Component/Productdetail.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
+import "./Productdetails.css";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./Productdetails.css";
 
 
 
