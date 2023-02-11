274d43af776f26a782c11d13ed380838d81344c7
Aniket Pratap Singh <103597886+createaniket@users.noreply.github.com>
2023-02-11 09:57:59 +0530

    about enhanced

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-
+import aniketimage from '../Assets/aniketbestpic.jpeg'
 const About = () => {
   return (
-    <div className='container' style={{height:"100vh"}}>
+    <div className='container' style={{height:"80vh" , display:"flex", justifyContent:"center"}}>
       
-      <div class="card my-4" style={{width: "18re"}}>
-  <img src="https://media-exp1.licdn.com/dms/image/C4E03AQH5YfHCNWLhqw/profile-displayphoto-shrink_200_200/0/1624131627840?e=1675296000&v=beta&t=9LrZHi5Hsfn3ddZNy7v_FxSOd3Qd8FD6-Xno34Q65nc" class="card-img-top" alt="..." style={{height:"46vh" , width:"27vw"}}/>
-  <div class="card-body">
-    <h5 class="card-title">About Me</h5>
+      <div className="card my-4" style={{width: "37vw"}}>
+  <img src={aniketimage} className="card-img-top" alt="..." style={{height:"46vh" , width:"27vw" }}/>
+  <div className="card-body">
+    <h5 className="card-title">About Me</h5>
     <h6>Hello , I am Aniket Pratap Singh <strong> "(MERN Developer)"</strong> </h6>
-    <p class="card-text">The creator owner and designer of this fully functioning Plant App</p>
+    <p className="card-text">The creator owner and designer of this fully functioning Plant App</p>
     <p> <strong> <bold> Thanks for visting here! any advice and suggestion are highly welcome!</bold></strong></p>
     <p> aniketpratapsingh19@gmail.com</p>
        
-        <a href="https://www.linkedin.com/in/aniket-pratap-singh-28669b193/" classname="btn btn-primary">Linked Profile</a>
+        <a href="https://www.linkedin.com/in/aniket-pratap-singh-28669b193/" classname="btn btn-primary">LinkedIn Profile</a>
   </div>
 </div>
     </div>
--- src/Component/plantItems.js
+++ src/Component/plantItems.js
@@ -3,9 +3,8 @@ import { useNavigate } from 'react-router-dom'
 import axios from "axios";
 const PlantItems = (props) => {
   const navigate = useNavigate();
-  let { title , description , price , productimg , _id} = props.product
+  let { title  , price , productimg , _id} = props.product
   const NewImage = `https://plant-shop-production.up.railway.app/${productimg[0]}`
-  const maindesc = description.slice(0 , 73)
   const sendtodetail = async()=>{
     let webApiUrl = `https://plant-shop-production.up.railway.app/product/get/${_id}`;
     const response =    await  axios.get(webApiUrl);
@@ -16,8 +15,8 @@ const PlantItems = (props) => {
             <div classname="card" onClick={ () =>{sendtodetail()}} style={{width: "25rem" ,  border:"1px solid gray" , height:"51vh" , margin:"19px" , borderRadius:"10px", backgroundColor:"white"}}>
              <img src={NewImage} class="img-thumbnail" alt="Cinque Terre" style={{height:"37vh" , border:"0"}}></img>
             <div classname="card-body" >
-                <h5 classname="card-title" style={{ transform: "translateX(10%)"}}>{title}</h5>
-                <p classname="card-text"style={{ transform: "translateX(3%)"}}>{maindesc}...</p>
+                <h5 classname="card-title" style={{ transform: "translateX(10%)" , fontWeight:"bolder"}}>{title}</h5>
+                <p style={{ transform: "translateX(10%)" , fontWeight:"bolder" , margin:'0', padding:'0'}}> Rating : 4.5</p>
                 <p classname="card-text"style={{ transform: "translateX(10%)" , color:"#100f0f", fontSize:"150%"}}> Rs. {price}</p>
             </div>
             </div>
